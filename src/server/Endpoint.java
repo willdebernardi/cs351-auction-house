91ab7cff7801222eb51f4e4aba67f908ed8ddf0c
Isaiah Martell <imartell@unm.edu>
2021-04-28 17:40:54 -0600

    Server and request done

--- src/server/Endpoint.java
+++ src/server/Endpoint.java
@@ -2,7 +2,7 @@
  * Represents an endpoint for the server API that accepts a request and returns
  * a response.
  *
- * @author Christopher Medlin, Will Debernardi, Isaac Martell
+ * @author Christopher Medlin, Will Debernardi, Isaiah Martell
  * @date 26 Apr 2021
  */
 package server;
