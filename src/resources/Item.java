cd6f2135d0ce6b7dd3727a7f6e3665caa6abc005
Will Debernardi <wdebernardi@unm.edu>
2021-05-04 18:46:40 -0600

    Implement basic Item resource, endpoint to list specific item, and endpoint to view all items

---
+++ src/resources/Item.java
@@ -0,0 +1,24 @@
+package resources;
+
+public class Item {
+    private String name;
+    private int highestBid;
+    private int bidderId;
+
+    public Item(String name, int highestBid, int bidderId) {
+        this.name = name;
+        this.highestBid = highestBid;
+        this.bidderId = bidderId;
+    }
+
+    public Item placeBid(int amount) {
+        if (amount < this.highestBid) {
+            throw new IllegalStateException("Bid too low.");
+        }
+        return new Item(this.name, amount, this.bidderId);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
