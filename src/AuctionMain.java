946f5423bb2c3b1d5330b71ed537b86471ea826a
Will Debernardi <wdebernardi@unm.edu>
2021-05-08 14:48:44 -0600

    Add item generation

--- src/AuctionMain.java
+++ src/AuctionMain.java
@@ -6,8 +6,14 @@ import server.*;
 import server.store.DataStore;
 import server.store.Resource;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
 public class AuctionMain {
     public static void main(String[] args) {
@@ -33,8 +39,11 @@ public class AuctionMain {
                 "ip", hostAddress, "port", hostPort));
         Response r = client.waitForResponse();
 
-        Resource items = new Resource<Item>("items",
-                () -> new Item("",0 ,-1, -1));
+        Resource items = new Resource<Item>("items", AuctionMain::generateItem);
+        int randomNumber = new Random().nextInt(7);
+        for(int i = 0; i < randomNumber; i++) {
+            items.create();
+        }
         Resource auctionAccountId = new Resource<Integer>("auctionId",
                 () -> 0);
         int id = auctionAccountId.create();
@@ -48,5 +57,34 @@ public class AuctionMain {
                 new ItemsList()));
         server.addEndpoint(new Endpoint("items.bid",
                 new Bid(address, port)));
+    }
+
+    public static Item generateItem() {
+        FileInputStream nouns = null;
+        FileInputStream adjectives = null;
+        try {
+            nouns = new FileInputStream(String.valueOf(
+                    AuctionMain.class.getResource("/resources/nouns.txt")));
+            adjectives = new FileInputStream(String.valueOf(
+                    AuctionMain.class.getResource("/resources/adjectives.txt")));
+        } catch (FileNotFoundException e) {
+            System.out.println("world broke");
+        }
+
+        ArrayList<String> nounList = new ArrayList<>();
+        ArrayList<String> adjectList = new ArrayList<>();
+        Scanner nounScan = new Scanner(nouns);
+        while(nounScan.hasNext()) {
+            nounList.add(nounScan.nextLine());
+        }
+        Scanner adjectScan = new Scanner(adjectives);
+        while(adjectScan.hasNext()) {
+            adjectList.add(adjectScan.nextLine());
+        }
+
+        Collections.shuffle(nounList);
+        Collections.shuffle(adjectList);
+        String itemName = adjectList.get(0) + " " + nounList.get(0);
+        return new Item(itemName, 0, -1, -1);
     }
 }
